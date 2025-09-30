1e594f820699011be1fa6df509fef88d782b669c
martinmbuba <martinmbuba4@gmail.com>
2025-09-30 13:08:59 +0300

    WMRF-3 create login page

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './components/Homepage';
 import Register from './components/Registerpage';
+import Login from './components/Loginpage';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/register" element={<Register />} /> 
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
         <Route path="*" element={<div>404 - Page Not Found</div>} />
       </Routes>
     </Router>
