d92205648564404eca2147d2bf81e7c5d35919a6
Umurowoto <umurowoto132@gmail.com>
2025-09-30 16:44:30 +0300

    grid and mole appearance

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './components/Homepage';
 import Register from './components/Registerpage';
 import Login from './components/Loginpage';
+import Game from './components/Game';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/game" element={<Game />} />
         <Route path="*" element={<div>404 - Page Not Found</div>} />
       </Routes>
     </Router>
